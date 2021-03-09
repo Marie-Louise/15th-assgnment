30f2d7beb9b889b359e7385738b5a455972f481f
Marie-Louise <mariemuteteyimana@gmail.com>
2021-03-09 22:37:48 +0300

    15th assgnment

---
+++ src/causin.kt
@@ -0,0 +1,29 @@
+fun main() {
+    name()
+    modulus()
+    sum()
+    personality()
+
+
+}
+
+fun name(){
+    println("marie louise")
+}
+fun modulus(){
+    var num1=20
+    var num2=3
+    var modulus=num1%num2
+    println(modulus)
+}
+fun sum(){
+    var num1=6
+    var num2=8
+    var num3=4
+    var num4=8
+    var sum=num1+num2+num3+num4
+    println(sum)
+}
+fun personality(){
+    println("risk taking")
+}
